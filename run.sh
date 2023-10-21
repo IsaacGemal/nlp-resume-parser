c0c9b88bfc98ebd1b7ecc193c747e90be832b7fc
Isaac <147355120+IsaacGemal@users.noreply.github.com>
2023-10-20 22:37:09 -0400

    Removed hardcoded api key Removed run.sh Added run_server.bat Added run_server - with key.bat (included in .gitignore) Added comments to regex updated engine to gpt-3.5-turbo-instruct

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 export RESUME_PARSER_HOST=127.0.0.1
 export RESUME_PARSER_PORT=8080
-export OPENAI_API_KEY=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
+export OPENAI_API_KEY=
 
 cd application
 echo Parser Running at $RESUME_PARSER_HOST:$RESUME_PARSER_PORT
